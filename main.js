fc0208211b09fd7bbdc62230fdf9965584940d92
helloPiggs <r.renaldo5@gmail.com>
2023-04-02 19:10:05 -0400

    files for senior show website (04/02/23)

---
+++ main.js
@@ -0,0 +1,303 @@
+// function start() {
+//     document.getElementById("jamesA").style.display = "none";
+// }
+let opened = false;
+
+function jamesA() {
+    if (!opened) {
+        document.getElementById("jamesA").style.display = "block";
+        document.getElementById("jamesABorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("jamesA").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("jamesABorder").style.borderTop = "none";
+        document.getElementById("jamesA").style.display = "none";
+    }
+}
+
+function frankieB() {
+    if (!opened) {
+        document.getElementById("frankieB").style.display = "block";
+        document.getElementById("frankieBBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("frankieB").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("frankieBBorder").style.borderTop = "none";
+        document.getElementById("frankieB").style.display = "none";
+    }
+}
+
+function rolenC() {
+    if (!opened) {
+        document.getElementById("rolenC").style.display = "block";
+        document.getElementById("rolenCBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("rolenC").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("rolenCBorder").style.borderTop = "none";
+        document.getElementById("rolenC").style.display = "none";
+    }
+}
+
+function habibDK() {
+    if (!opened) {
+        document.getElementById("habibDK").style.display = "block";
+        document.getElementById("habibDKBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("habibDK").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("habibDKBorder").style.borderTop = "none";
+        document.getElementById("habibDK").style.display = "none";
+    }
+}
+
+function collinF() {
+    if (!opened) {
+        document.getElementById("collinF").style.display = "block";
+        document.getElementById("collinFBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("collinF").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("collinFBorder").style.borderTop = "none";
+        document.getElementById("collinF").style.display = "none";
+    }
+}
+
+function jakeF() {
+    if (!opened) {
+        document.getElementById("jakeF").style.display = "block";
+        document.getElementById("jakeFBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("jakeF").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("jakeFBorder").style.borderTop = "none";
+        document.getElementById("jakeF").style.display = "none";
+    }
+}
+
+function giacomoL() {
+    if (!opened) {
+        document.getElementById("giacomoL").style.display = "block";
+        document.getElementById("giacomoLBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("giacomoL").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("giacomoLBorder").style.borderTop = "none";
+        document.getElementById("giacomoL").style.display = "none";
+    }
+}
+
+function rayAnnM() {
+    if (!opened) {
+        document.getElementById("rayAnnM").style.display = "block";
+        document.getElementById("rayAnnMBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("rayAnnM").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("rayAnnMBorder").style.borderTop = "none";
+        document.getElementById("rayAnnM").style.display = "none";
+    }
+}
+
+function russellR() {
+    if (!opened) {
+        document.getElementById("russellR").style.display = "block";
+        document.getElementById("russellRBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("russellR").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("russellRBorder").style.borderTop = "none";
+        document.getElementById("russellR").style.display = "none";
+    }
+}
+
+function benT() {
+    if (!opened) {
+        document.getElementById("benT").style.display = "block";
+        document.getElementById("benTBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("benT").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("benTBorder").style.borderTop = "none";
+        document.getElementById("benT").style.display = "none";
+    }
+}
+
+function tatiannaT() {
+    if (!opened) {
+        document.getElementById("tatiannaT").style.display = "block";
+        document.getElementById("tatiannaTBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("tatiannaT").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("tatiannaTBorder").style.borderTop = "none";
+        document.getElementById("tatiannaT").style.display = "none";
+    }
+}
+
+function juniW() {
+    if (!opened) {
+        document.getElementById("juniW").style.display = "block";
+        document.getElementById("juniWBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("juniW").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("juniWBorder").style.borderTop = "none";
+        document.getElementById("juniW").style.display = "none";
+    }
+}
+
+function redA() {
+    if (!opened) {
+        document.getElementById("redA").style.display = "block";
+        document.getElementById("redABorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("redA").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("redABorder").style.borderTop = "none";
+        document.getElementById("redA").style.display = "none";
+    }
+}
+
+function blakeA() {
+    if (!opened) {
+        document.getElementById("blakeA").style.display = "block";
+        document.getElementById("blakeABorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("blakeA").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("blakeABorder").style.borderTop = "none";
+        document.getElementById("blakeA").style.display = "none";
+    }
+}
+
+function isaiahB() {
+    if (!opened) {
+        document.getElementById("isaiahB").style.display = "block";
+        document.getElementById("isaiahBBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("isaiahB").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("isaiahBBorder").style.borderTop = "none";
+        document.getElementById("isaiahB").style.display = "none";
+    }
+}
+
+function joelleB() {
+    if (!opened) {
+        document.getElementById("joelleB").style.display = "block";
+        document.getElementById("joelleBBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("joelleB").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("joelleBBorder").style.borderTop = "none";
+        document.getElementById("joelleB").style.display = "none";
+    }
+}
+
+function lucaE() {
+    if (!opened) {
+        document.getElementById("lucaE").style.display = "block";
+        document.getElementById("lucaEBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("lucaE").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("lucaEBorder").style.borderTop = "none";
+        document.getElementById("lucaE").style.display = "none";
+    }
+}
+
+function aidanF() {
+    if (!opened) {
+        document.getElementById("aidanF").style.display = "block";
+        document.getElementById("aidanFBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("aidanF").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("aidanFBorder").style.borderTop = "none";
+        document.getElementById("aidanF").style.display = "none";
+    }
+}
+
+function justinH() {
+    if (!opened) {
+        document.getElementById("justinH").style.display = "block";
+        document.getElementById("justinHBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("justinH").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("justinHBorder").style.borderTop = "none";
+        document.getElementById("justinH").style.display = "none";
+    }
+}
+
+function emmaJ() {
+    if (!opened) {
+        document.getElementById("emmaJ").style.display = "block";
+        document.getElementById("emmaJBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("emmaJ").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("emmaJBorder").style.borderTop = "none";
+        document.getElementById("emmaJ").style.display = "none";
+    }
+}
+
+function emmaL() {
+    if (!opened) {
+        document.getElementById("emmaL").style.display = "block";
+        document.getElementById("emmaLBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("emmaL").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("emmaLBorder").style.borderTop = "none";
+        document.getElementById("emmaL").style.display = "none";
+    }
+}
+
+function meghanM() {
+    if (!opened) {
+        document.getElementById("meghanM").style.display = "block";
+        document.getElementById("meghanMBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("meghanM").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("meghanMBorder").style.borderTop = "none";
+        document.getElementById("meghanM").style.display = "none";
+    }
+}
+
+function devinO() {
+    if (!opened) {
+        document.getElementById("devinO").style.display = "block";
+        document.getElementById("devinOBorder").style.borderTop = "3px rgb(231, 196, 62) solid";
+        document.getElementById("devinO").style.borderBottom = "3px rgb(231, 196, 62) solid";
+        opened = true;
+    } else {
+        opened = false;
+        document.getElementById("devinOBorder").style.borderTop = "none";
+        document.getElementById("devinO").style.display = "none";
+    }
+}
